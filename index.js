96069c78dc4ed7a93e6125e66cd60b265f0fc48a
carrot10pumpkin <jeijames@proton.me>
2024-05-06 17:50:45 -0400

    Added .env

--- index.js
+++ index.js
@@ -2,23 +2,57 @@ const express = require('express');
 const path = require('path');
 const app = express();
 var contentful = require('contentful');
+require("dotenv").config();
 // main.js
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// requesting info 
+
+
+
+var Items = []
+
 
 var client = contentful.createClient({
-    space: 'v8crlmuqp7e9',
-    accessToken: 'vRcJku13pQF8l64elTwBkzeM22iI1J9P_ti7nmdOjdY',
+    space: process.env.SPACE_ID  ,
+    accessToken: process.env.ACCESS_TOKEN,
   });
-  client.getEntries().then(function (entries) {
-    // log the title for all the entries that have it
-    entries.items.forEach(function (entry) {
-      if (entry.fields.productName) {
-        console.log(entry.fields.productName);
-      }
-    });
+
+
+
+client.getEntries().then(function (entries) {
+  
+  entries.items.forEach(function (entry) {
+
+    if (entry.fields.productName) {
+      console.log(entry.fields.productName);
+      Items.push(entry)
+    }
   });
+});
+
+
+
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', async(req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+
+
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013},
+   ...Items
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+    res.render('pages/home', {
+      Items,
+    mascots: mascots,
+    tagline: tagline
+  });
    
 });
 app.listen(8080, () => {
